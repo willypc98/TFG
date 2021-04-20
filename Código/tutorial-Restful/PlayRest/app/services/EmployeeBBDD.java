c6d70dcc408096e2e5f0d53a9806027210297c47
willypc98 <willpc98@gmail.com>
2021-04-20 18:57:25 +0200

    Creando una primera conexión a la base de datos

--- Código/tutorial-Restful/PlayRest/app/services/EmployeeBBDD.java
+++ Código/tutorial-Restful/PlayRest/app/services/EmployeeBBDD.java
@@ -6,9 +6,63 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.inject.*;
+import play.db.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class EmployeeBBDD {
 
-/*
+    Database database = Databases.createFrom("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/tfg");
+
+    private static Connection con;
+    private static final String driver="com.mysql.jdbc.Driver";
+    private static final String user="root";
+    private static final String pass="root";
+    private static final String url="jdbc:mysql://localhost:3306/tfg";
+
+
+    private Statement createStatement;
+
+    private ResultSet rS;
+
+    protected boolean conector() throws SQLException, ClassNotFoundException {
+        // Reseteamos a null la conexion a la bd
+        con=null;
+        boolean valor=false;
+        try{
+
+            int i=0;
+            //  String valor= "fallo";
+            Class.forName(driver);
+            // Nos conectamos a la bd
+            con= (com.mysql.jdbc.Connection) DriverManager.getConnection(url, user, pass);
+            // Si la conexion fue exitosa mostramos un mensaje de conexion exitosa
+            if (con!=null){
+
+                createStatement = con.createStatement();
+                valor= true;
+            }
+        }
+        //Si la conexion NO fue exitosa mostramos un mensaje de error
+        catch (ClassNotFoundException | SQLException e){
+            System.out.println( e);
+        }
+        return  valor;
+    }
+
+
+
+
+
+    /*
     private static EmployeeService instance;
     private Map<Integer, Employee> employees = new HashMap<>();
 
