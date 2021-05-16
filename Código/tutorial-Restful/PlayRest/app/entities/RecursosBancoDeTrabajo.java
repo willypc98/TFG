1dd2ad8a59da7c41c8d9a85873a27250057a87c5
willypc98 <willpc98@gmail.com>
2021-05-16 18:40:25 +0200

    Primera versión RecursosBancoDeTrabajo

--- Código/tutorial-Restful/PlayRest/app/entities/RecursosBancoDeTrabajo.java
+++ Código/tutorial-Restful/PlayRest/app/entities/RecursosBancoDeTrabajo.java
@@ -1,4 +1,32 @@
 package entities;
 
 public class RecursosBancoDeTrabajo {
+
+    private int idRecursoBanco;
+    private String nombreRecursoBanco;
+    private String desccripcionRecursoBanco;
+
+    public int getIdRecursoBanco() {
+        return idRecursoBanco;
+    }
+
+    public void setIdRecursoBanco(int idRecursoBanco) {
+        this.idRecursoBanco = idRecursoBanco;
+    }
+
+    public String getNombreRecursoBanco() {
+        return nombreRecursoBanco;
+    }
+
+    public void setNombreRecursoBanco(String nombreRecursoBanco) {
+        this.nombreRecursoBanco = nombreRecursoBanco;
+    }
+
+    public String getDesccripcionRecursoBanco() {
+        return desccripcionRecursoBanco;
+    }
+
+    public void setDesccripcionRecursoBanco(String desccripcionRecursoBanco) {
+        this.desccripcionRecursoBanco = desccripcionRecursoBanco;
+    }
 }
