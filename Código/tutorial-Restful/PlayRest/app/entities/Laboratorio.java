c8650a6186987e92b1364eabd4da671ee459378b
willypc98 <willpc98@gmail.com>
2021-05-16 18:34:20 +0200

    Primera versión Laboratorio

--- Código/tutorial-Restful/PlayRest/app/entities/Laboratorio.java
+++ Código/tutorial-Restful/PlayRest/app/entities/Laboratorio.java
@@ -1,4 +1,43 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Laboratorio {
+
+    private int idLab;
+    private String nombreLab;
+    private String desccripcionLab;
+    private ArrayList<String> listaDisponibilidad;
+
+    public int getIdLab() {
+        return idLab;
+    }
+
+    public void setIdLab(int idLab) {
+        this.idLab = idLab;
+    }
+
+    public String getNombreLab() {
+        return nombreLab;
+    }
+
+    public void setNombreLab(String nombreLab) {
+        this.nombreLab = nombreLab;
+    }
+
+    public String getDesccripcionLab() {
+        return desccripcionLab;
+    }
+
+    public void setDesccripcionLab(String desccripcionLab) {
+        this.desccripcionLab = desccripcionLab;
+    }
+
+    public ArrayList<String> getListaDisponibilidad() {
+        return listaDisponibilidad;
+    }
+
+    public void setListaDisponibilidad(ArrayList<String> listaDisponibilidad) {
+        this.listaDisponibilidad = listaDisponibilidad;
+    }
 }
