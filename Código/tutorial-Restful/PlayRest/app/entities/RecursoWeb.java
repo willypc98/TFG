0e860649db6156a7cb07dca40a93d7124d5855ab
willypc98 <willpc98@gmail.com>
2021-05-20 11:37:00 +0200

    Creación de la súper clase RecursoWeb para crear una jerarquía de herencia

---
+++ Código/tutorial-Restful/PlayRest/app/entities/RecursoWeb.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class RecursoWeb {
+
+    private int id;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "RecursoWeb{" +
+                "id=" + id +
+                ", URL='" + url  +
+
+                '}';
+    }
+}
