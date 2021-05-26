64af54ffb373f74a811a939920407af40af62de1
willypc98 <willpc98@gmail.com>
2021-05-26 18:41:05 +0200

    Primera versión comentada de LaboratorioBBDD que falta por meter los 2 arraylist en la BBDD

--- Código/tutorial-Restful/PlayRest/app/services/LaboratorioBBDD.java
+++ Código/tutorial-Restful/PlayRest/app/services/LaboratorioBBDD.java
@@ -1,4 +1,197 @@
 package services;
 
+
+import entities.Laboratorio;
+import entities.Laboratorio;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LaboratorioBBDD extends ConexionBBDD{
+
+    private static LaboratorioBBDD instance;
+    public static LaboratorioBBDD getInstance() {
+        if (instance == null) {
+            instance = new LaboratorioBBDD();
+        }
+        return instance;
+    }
+/*
+    public Laboratorio addLaboratorio(Laboratorio lab) throws SQLException, ClassNotFoundException {
+        if (conector() == true) {
+
+            int id = lab.getId();
+            String url = lab.getUrl();
+
+
+
+            createStatement.executeUpdate("INSERT INTO laboratorio (id,url,nombre,grado) VALUES ("+id+", '" + url + "', '" + nombre + "', '" + grado + "')");
+            con.close();
+
+        }
+        return lab;
+    }
+
+    public Laboratorio getLaboratorio(int id) {
+        Laboratorio lab = new Laboratorio();
+        try {
+            if(conector()==true){
+
+                String queryBBDD = "select * from laboratorio where id=" + id + ";";
+                int i=0;
+                try {
+                    rS = createStatement.executeQuery(queryBBDD);
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (rS == null){
+                    lab= null;
+
+                }
+                else{
+
+                    try {
+                        while (rS.next()) {
+                            lab.setId(rS.getInt("id"));
+                            lab.setUrl(rS.getString("url"));
+                            lab.setNombre(rS.getString("nombre"));
+                            lab.setGrado(rS.getString("grado"));
+
+
+                        }
+                    } catch (SQLException ex) {
+                        Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    try {
+                        i = 0;
+                        con.close();
+                    } catch (SQLException ex) {
+                        Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+            }
+            else{
+                lab=null;
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EmployeeBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lab;
+    }
+
+    public ArrayList<Laboratorio> getAllLaboratorios() {
+        ArrayList<Laboratorio> laboratoriosLista = new ArrayList();
+        try {
+            if(conector()==true){
+                String queryBBDD = "select * from laboratorio;";
+                int i=0;
+                try {
+                    rS = createStatement.executeQuery(queryBBDD);
+
+                    while (rS.next()) {
+                        Laboratorio lab = new Laboratorio();
+                        lab.setId(Integer.parseInt(rS.getString("id")));
+                        lab.setUrl(rS.getString("url"));
+                        lab.setNombre(rS.getString("nombre"));
+                        lab.setGrado(rS.getString("grado"));
+                        laboratoriosLista.add(lab);
+
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                try {
+                    i=0;
+                    con.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+            else{
+                return laboratoriosLista;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EmployeeBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("El tamaño de la lista es" + laboratoriosLista.size());
+        return laboratoriosLista;
+
+    }
+    public Laboratorio updateLaboratorio(Laboratorio lab ) throws SQLException, ClassNotFoundException {
+        try {
+            if (conector() == true) {
+                int id = lab.getId();
+                //String url = lab.getUrl();
+                String nombre = lab.getNombre();
+                String grado= lab.getGrado();
+
+                String queryBBDD = "update Laboratorio set nombre='"+nombre+"', grado='"+grado+"' where id="+id+";";
+
+                try {
+                    createStatement.executeUpdate(queryBBDD);
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                try {
+
+                    con.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            else{
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lab;
+    }
+
+
+    public boolean deleteLaboratorio(int id) throws SQLException, ClassNotFoundException {
+        boolean valor= false;
+        try {
+            if (conector() == true) {
+
+                String queryBBDD = "delete from laboratorio where id="+id+";";
+
+                try {
+                    createStatement.executeUpdate(queryBBDD);
+                    valor = true;
+                    return valor;
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                try {
+
+                    con.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            else{
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LaboratorioBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return valor;
+    }
+    
+ */
 }
