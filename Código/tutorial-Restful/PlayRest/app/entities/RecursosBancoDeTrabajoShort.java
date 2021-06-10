4bb0b987bdd54bad4a2aa8c20731ca2fbe7ade39
willypc98 <willpc98@gmail.com>
2021-06-10 13:00:04 +0200

    Añadiendo los tributos de RecursosBancoDeTrabajoShort

--- Código/tutorial-Restful/PlayRest/app/entities/RecursosBancoDeTrabajoShort.java
+++ Código/tutorial-Restful/PlayRest/app/entities/RecursosBancoDeTrabajoShort.java
@@ -1,4 +1,32 @@
 package entities;
 
 public class RecursosBancoDeTrabajoShort extends RecursoWeb{
+
+    private String nombreRecursoBanco;
+    private String descripcionRecursoBanco;
+    private int labID;
+    private int bancoID;
+
+    public RecursosBancoDeTrabajoShort(){
+        super();
+    }
+
+    public RecursosBancoDeTrabajoShort(int id, String url, String nombreRecursoBanco, String descripcionRecursoBanco, int bancoID, int labID ) {
+        super(id, url);
+        this.nombreRecursoBanco=nombreRecursoBanco;
+        this.descripcionRecursoBanco=descripcionRecursoBanco;
+        this.labID=labID;
+        this.bancoID=bancoID;
+
+    }
+
+    public int getLabID() { return labID; }
+
+    public void setLabID(int labID) { this.labID = labID; }
+
+    public int getBancoID() { return bancoID; }
+
+    public void setBancoID(int bancoID) { this.bancoID = bancoID; }
+
+
 }
