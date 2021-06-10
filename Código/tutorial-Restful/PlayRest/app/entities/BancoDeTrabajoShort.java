b8a1f9729f97b9061c9a43c4e8b11ca81539760a
willypc98 <willpc98@gmail.com>
2021-06-10 12:57:10 +0200

    Añadiendo los atributos de BancoDeTrabajoShort

--- Código/tutorial-Restful/PlayRest/app/entities/BancoDeTrabajoShort.java
+++ Código/tutorial-Restful/PlayRest/app/entities/BancoDeTrabajoShort.java
@@ -1,4 +1,33 @@
 package entities;
 
 public class BancoDeTrabajoShort extends RecursoWeb{
+    private String descripcionBanco;
+    private int labID;
+
+    public BancoDeTrabajoShort(){
+        super();
+    }
+
+    public BancoDeTrabajoShort(int id, String url, String descripcionBanco, int labID) {
+        super(id, url);
+        this.descripcionBanco=descripcionBanco;
+        this.labID=labID;
+    }
+
+    public String getDescripcionBanco() {
+        return descripcionBanco;
+    }
+
+    public void setDescripcionBanco(String desccripcionBanco) {
+        this.descripcionBanco = desccripcionBanco;
+    }
+
+
+    public int getLabID() {
+        return labID;
+    }
+
+    public void setLabID(int labID) {
+        this.labID = labID;
+    }
 }
