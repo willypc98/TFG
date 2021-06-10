a92e238b761c28419c52a99740788474f2e0c198
willypc98 <willpc98@gmail.com>
2021-06-10 12:55:49 +0200

    Añadiendo los atributos de LaboratorioShort

--- Código/tutorial-Restful/PlayRest/app/entities/LaboratorioShort.java
+++ Código/tutorial-Restful/PlayRest/app/entities/LaboratorioShort.java
@@ -1,4 +1,35 @@
 package entities;
 
 public class LaboratorioShort extends RecursoWeb{
+
+    private String nombreLab;
+    private String descripcionLab;
+
+    public LaboratorioShort(){
+        super();
+
+    }
+    public LaboratorioShort(int id, String url, String nombreLab, String descripcionLab) {
+        super(id, url);
+        this.nombreLab=nombreLab;
+        this.descripcionLab=descripcionLab;
+
+    }
+
+    public String getNombreLab() {
+        return nombreLab;
+    }
+
+    public void setNombreLab(String nombreLab) {
+        this.nombreLab = nombreLab;
+    }
+
+    public String getDescripcionLab() {
+        return descripcionLab;
+    }
+
+    public void setDescripcionLab(String descripcionLab) {
+        this.descripcionLab = descripcionLab;
+    }
+
 }
