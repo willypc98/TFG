f76678ceab68db075df2f9876c5f263c1bd20976
willypc98 <willpc98@gmail.com>
2021-06-21 19:24:08 +0200

    Se han borrado algunas cosas que sobraban

--- Código/tutorial-Restful/PlayRest/app/views/Freemarker.java
+++
@@ -1,8 +0,0 @@
-package views;
-import freemarker.cache.*;
-import freemarker.ext.beans.*;
-import freemarker.template.*;
-public class Freemarker {
-
-    private static freemarker.template.Configuration cfg = new freemarker.template.Configuration();
-}
